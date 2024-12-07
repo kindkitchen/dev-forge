1a5f18e50c6c6e9ba29f12d509ed552fd5887c47
Nik Kita <nik-kita@on.github>
2024-12-07 20:43:45 +0100

    build formkit-theme-deno and serve it as static spa

--- main.tsx
+++ main.tsx
@@ -1,14 +1,22 @@
+import { load } from "@std/dotenv";
+import { join } from "@std/path";
 import { Hono } from "hono";
 import { serveStatic } from "hono/deno";
 
+const config = {
+  PORT: 3000,
+  ...await load() as Partial<Record<"FORMKIT_THEME_DENO" | "PORT", string>>,
+};
 const app = new Hono();
+const root = join("formkit-theme-deno", "dist");
+console.log(root);
 
-app.use("/formkit-theme-deno/*", serveStatic({ root: "./formkit-theme-deno" }));
+app.use("/*", serveStatic({ root }));
 
-app.get("/", (c) => {
-  return c.html(<h1>Hi!</h1>);
-});
+// app.get("/", (c) => {
+//   return c.html(<h1>Hi!</h1>);
+// });
 
 Deno.serve({
-  port: 3000,
+  port: +config.PORT,
 }, app.fetch);
